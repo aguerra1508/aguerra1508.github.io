6d39f556f5c0d7e852a10df5fd84953a888a3846
Analee Guerra <aguerra1508@gmail.com>
2020-08-11 01:58:28 -0500

    Fixed countdown timer: when time = 0, text = time expired.

--- Homework/04-Homework/javascript.js
+++ Homework/04-Homework/javascript.js
@@ -1,27 +1,35 @@
 //HTML document variables
-var startQuizBtn = document.getElementById("button");
+var startQuizBtn = document.getElementById("start-button");
 var timer = document.getElementById("timer");
-var start = document.getElementById("start-card");
-var questions = document.getElementById("questions-card");
+var startCard = document.getElementById("start-card");
+var questionsCard = document.getElementById("questions-card");
+var questionText = document.getElementById("question-text");
+var choiceA = document.getElementById("choice-a");
+var choiceB = document.getElementById("choice-b");
+var choiceC = document.getElementById("choice-c");
+var choiceD = document.getElementById("choice-d");
+var nextBtn = document.getElementById ("next-button");
+var questions = "What does HTML stand for?"
+var answers = "Hyper Text Mark Up Language"
 
 //Function to hide start card when start quiz button clicked
 function hideStart() {
-  if (start.style.display === "none") {
-    start.style.display = "block";
+  if (startCard.style.display === "none") {
+    startCard.style.display = "block";
   } else {
-    start.style.display = "none";
+    startCard.style.display = "none";
   }
 }
 
 //Function to keep questions hidden until start button clicked
 function hiddenQuestionsUntilStart() {
-  questions.style.display = "block";
+  questionsCard.style.display = "block";
 }
 
 //Function to start timer when start button clicked
 function startTimer() {
-  var eightMin = 8 * 60;
-  setInterval(function () {
+  var eightMin = 1 * 60;
+  var timerCount = setInterval(function () {
       eightMin--
       var minutes = Math.floor(eightMin / 60);
       var seconds = Math.floor(eightMin % 60);
@@ -29,9 +37,15 @@ function startTimer() {
         seconds = "0" + seconds
       }
       timer.textContent = "Time Left: " + minutes + ":" + seconds;
-      
+      if (eightMin === 0) {
+        clearInterval(timerCount);
+        timer.textContent=  "TIME EXPIRED!";
+      }
   }, 1000)
 }
+
+questionText.append(questions);
+choiceA.append(answers);
 
 //Event listeners to hide start card, unhide questions and start timer upon click of the start button
 startQuizBtn.addEventListener ("click", startTimer);
