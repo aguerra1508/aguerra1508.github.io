9d78c1e81ca47758feec0daf2cc3bdd9b9007204
Analee Guerra <aguerra1508@gmail.com>
2020-08-11 00:37:55 -0500

    Styling changes

--- Homework/04-Homework/javascript.js
+++ Homework/04-Homework/javascript.js
@@ -1,8 +1,10 @@
+//HTML document variables
 var startQuizBtn = document.getElementById("button");
 var timer = document.getElementById("timer");
 var start = document.getElementById("start-card");
 var questions = document.getElementById("questions-card");
 
+//Function to hide start card when start quiz button clicked
 function hideStart() {
   if (start.style.display === "none") {
     start.style.display = "block";
@@ -11,21 +13,27 @@ function hideStart() {
   }
 }
 
-function hiddenQuestions() {
+//Function to keep questions hidden until start button clicked
+function hiddenQuestionsUntilStart() {
   questions.style.display = "block";
 }
 
+//Function to start timer when start button clicked
 function startTimer() {
   var eightMin = 8 * 60;
   setInterval(function () {
       eightMin--
-      var minutes = eightMin / 60;
-      var seconds = eightMin % 60;
-
-      timer.textContent = (Math.floor(minutes)) + " : " + (Math.floor(seconds));
+      var minutes = Math.floor(eightMin / 60);
+      var seconds = Math.floor(eightMin % 60);
+      if (seconds < 10) {
+        seconds = "0" + seconds
+      }
+      timer.textContent = "Time Left: " + minutes + ":" + seconds;
+      
   }, 1000)
 }
 
+//Event listeners to hide start card, unhide questions and start timer upon click of the start button
 startQuizBtn.addEventListener ("click", startTimer);
 startQuizBtn.addEventListener ("click", hideStart);
-startQuizBtn.addEventListener("click", hiddenQuestions);
+startQuizBtn.addEventListener("click", hiddenQuestionsUntilStart);
