2a95aa53bb3ec230a49c1672f13c29ab916e7f5d
Analee Guerra <aguerra1508@gmail.com>
2020-08-22 08:41:43 -0500

    Added comments and minor changes to CSS styling

--- Homework/05-Homework/Develop/jquery.js
+++ Homework/05-Homework/Develop/jquery.js
@@ -1,5 +1,8 @@
+//Document ready function
 $(document).ready(function () {
+    //Current day with moment js data
     $("#currentDay").text(moment().format('LL'));
+    //Work day object with hour display and military time for moment js
     var workDay = [{
             hour: "9AM",
             time: 9,
@@ -37,14 +40,16 @@ $(document).ready(function () {
             time: 17,
         }
     ]
-
+    //For loop to create time blocks for work day object
     for (var i = 0; i < workDay.length; i++) {
+        //Create elements
         var newDiv = $("<div>");
         var hourDiv = $("<div>");
         var description = $("<textarea>");
         var save = $("<button>");
         var icon = $("<i>");
-
+        
+        //Add attributes which correspond with CSS style sheet
         newDiv.attr("class", "row time-block");
         newDiv.attr("id", workDay[i].hour);
         $(".container").append(newDiv);
@@ -54,8 +59,11 @@ $(document).ready(function () {
         hourDiv.text(workDay[i].hour);
 
         description.attr("class", "description col-10");
-        var eventText = localStorage.getItem(workDay[i].hour || "");
+        //Get local storage of items, if already present
+        var eventText = localStorage.getItem(workDay[i].hour);
+        //Display local storage items, if already present
         description.val(eventText);
+        //If, else if, else statement for changing of colors to indicate if past, present or future time
         if (workDay[i].time < moment().hour()) {
             description.addClass("past");
         } else if (workDay[i].time > moment().hour()){
@@ -72,13 +80,10 @@ $(document).ready(function () {
         icon.attr("class", "far fa-save");
         save.append(icon);
     };
+    //Save button function to set local storage
     $(".saveBtn").click(function() {
         var descripText = $(this).siblings("textarea").val();
         var timeText = $(this).siblings(".hour")[0].innerText;
         localStorage.setItem(timeText,descripText);
        });
 })
-
-//Current time: moment().format('LT');
-//moment().toString());
-//moment().hour();
