752951ef84bc776a979bce1bb8ed779eeb435693
Analee Guerra <aguerra1508@gmail.com>
2020-08-29 11:04:07 -0500

    removed current location search

--- Homework/06-Homework/script.js
+++ Homework/06-Homework/script.js
@@ -1,69 +1,6 @@
 //Open weather API key
 var apiKey = "0a788ecc2d25a7e8e1cbd39fd84dc58c";
 
-//Get current user location
-/*function getLocation() {
-  // Make sure browser supports this feature
-  if (navigator.geolocation) {
-    // Provide our showPosition() function to getCurrentPosition
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-}
-// This will get called after getCurrentPosition()
-function showPosition(position) {
-  // Grab coordinates from the given object
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  //API url for current location weather look up
-  var oneCallURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + apiKey;
-
-  //AJAX call for current city weather API
-  $.ajax({
-      url: oneCallURL,
-      method: "GET",
-    })
-    .then(function (response) {
-      console.log(response);
-      //Get response city name & add to HTML
-      var city = response.name
-      var selectedCity = document.getElementById("selected-city");
-      selectedCity.innerHTML = city + " (" + (moment().format('l')) + ")";
-      //Get response temperature, convert to F and add to HTML
-      var temp = document.getElementById("temperature");
-      var tempinK = response.main.temp;
-      var temptoF = (tempinK * (9 / 5) - 459.67)
-      //Round value
-      temptoF = Math.floor(temptoF);
-      temp.innerHTML = "Temperature: " + temptoF;
-      //Ger response humidity
-      var humidity = document.getElementById("humidity");
-      var cityHumidity = response.main.humidity;
-      humidity.innerHTML = "Humidity: " + cityHumidity;
-      //Get response wind speed
-      var windSpeed = document.getElementById("wind-speed");
-      var cityWind = response.wind.speed;
-      windSpeed.innerHTML = "Wind Speed: " + cityWind;
-      //URL for UV index
-      var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-
-      //AJAX for UV index for current city
-      $.ajax({
-          url: uvQueryURL,
-          method: "GET",
-        })
-        .then(function (responseUV) {
-          console.log(responseUV);
-          var uvIndex = document.getElementById("uv-index");
-          var cityUV = responseUV.value;
-          uvIndex.innerHTML = "UV Index: " + cityUV;
-        });
-    });
-
-}
-getLocation();*/
-
 //City search click function
 $("#search").on("click", function () {
   //Get input data
