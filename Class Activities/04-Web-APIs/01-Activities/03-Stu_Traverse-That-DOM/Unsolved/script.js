c1c790a8fa6721924150c401c80e13c76af39112
Analee Guerra <aguerra1508@gmail.com>
2020-09-15 19:20:49 -0500

    Cleaning up git hub & organizing

--- Class Activities/04-Web-APIs/01-Activities/03-Stu_Traverse-That-DOM/Unsolved/script.js
+++
@@ -1,4 +0,0 @@
-console.log(document.body);
-console.log(document.body.children[1]);
-var newArticles = document.body.children[1];
-newArticles.style.fontSize = "50px";
--- Class Activities/04-Web-APIs/01-Activities/05-Stu_Setting-Attributes/Unsolved/script.js
+++
@@ -1,6 +0,0 @@
-var iTags = document.querySelectorAll("img");
-for (var i = 0; i < iTags.length; i++){
-    iTags[i].setAttribute("src","images/image_1.jpg");
-    iTags[i].setAttribute("alt","alternate text");
-}
-
